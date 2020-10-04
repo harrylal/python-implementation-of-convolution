7b0de330937e210460ff46e76e2bfca7ad42edbd
harrylal <harrylal188@gmail.com>
2020-10-04 13:24:49 +0530

    update avgFilter.py

--- avgfilter.py
+++ avgfilter.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np 
-
+import time
 def genAvgKernal(kernSize): #genAvgKernal(kernSize) returns  a averaging filter kernal of size 'kernSize'
     avgKernal = np.ones((kernSize,kernSize),np.float32)/kernSize**2
     return avgKernal
@@ -67,12 +67,58 @@ def mirrorPad(img,filterSize):
 def getSize():
     return int(input("Enter filter size (example - input '3' to gen 3*3 filter): "))
 
-def menu():
-    menu = {1:'Mirroring' , 2:'Padding' , 3:'Crop-off' ,4:'Change Filter Size'}
+def mainMenu():
+    menu = {1:'Mirroring' , 2:'Zero padding' , 3:'Crop-off' ,4:'Change filter size',5:'Exit'}
     print("\n\n{:^40}\n{:^40}\n".format('MENU','======'))
     for key,option in menu.items():
         print('{:>17} {}\n'.format('('+str(key)+')',option))
-    
+    choice = int(input("Awaiting response.. : "))
+    if choice not in menu.keys():
+        print("invalid response!!")
+        time.sleep(2)
+        mainMenu()
+    return choice
   
-img_grey = cv2.imread('data/blur.tif',cv2.IMREAD_GRAYSCALE) 
-menu()
+img_grey = cv2.imread('data/gear.jpg',cv2.IMREAD_GRAYSCALE) 
+size = getSize()
+while True:
+    avgFilter = genAvgKernal(size)
+    response = mainMenu()
+    if response == 1 :
+        print("Mirror padding the image...")
+        mirrored_img = mirrorPad(img_grey,size)
+        print("Mirror Padding complete!!\nApplying filter...")
+        filtered_img = applyFilter(mirrored_img,avgFilter)
+        print("Filter Applied!!")
+    elif response == 2 :
+        print("Zero padding the image...")
+        zeropadded_img = zeroPad(img_grey,size)
+        print("Zero Padding complete!!\nApplying filter...")
+        filtered_img = applyFilter(zeropadded_img,avgFilter)
+        print("Filter Applied!!")
+    elif response == 3 :
+        print("Applying filter...")
+        filtered_img = applyFilter(img_grey,avgFilter)
+        print("Filter Applied image cropped!!")
+    elif response == 4 :
+        size = getSize()
+        continue
+    elif response == 5 :
+        print("Application closed")
+        exit()
+    
+    cv2.namedWindow('Raw Image',cv2.WINDOW_NORMAL)
+    cv2.namedWindow('Filtered Image',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Raw Image',800,800)
+    cv2.resizeWindow('Filtered Image',800,800)
+    
+
+    cv2.imshow("Raw Image",img_grey)
+    cv2.imshow("Filtered Image",filtered_img)
+    print("Press ESC to go back to menus")
+    key = cv2.waitKey(0)
+    if key ==  27:
+        cv2.destroyAllWindows()
+print("Application Closed")
+
+
